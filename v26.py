bcdff616d41c8b35c6e08183cd8359bf78b6af07
Mechanicalbird <mechanicalbird_just@yahoo.com>
2017-06-26 22:01:42 +0900

    Rename v25.py to v26.py

---
+++ v26.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+style.use('ggplot')
+
+class support_vector_Machine:
+    def __int__(self, visualization=True):
+        self.visualization = visualization
+        self.colors ={1:'r', -1:'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax =self.fig.add_subplot(1,1,1)
+
+    #train
+    def fit(self, data):
+        sefl.data = data
+	# { ||w||: [w,b] }
+	opt_dict ={}
+
+	transforms = [[1,1],
+                      [-1,1],
+                      [1,-1],
+                      [1,-1]]
+
+        all_data = []
+	for yi in sefl.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+
+        
+	self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+	step_size = [self.max_feature_value * 0.1,
+	             self.max_feature_value * 0.01,
+                     # point of expense:
+	             self.max_feature_value * 0.001,]
+
+        # extremely expensive
+	b_range_multiple = 5
+
+	#
+	b_multiple = 5
+
+	latest_optimum = self.max_feature_value*10
+
+	for step in step_sizes:
+            w = np.array([latest_optimum,latest_optimum])
+	    # we can do this because convex
+            optimized = False
+            while not optimized:
+                pass
+
+
+    def predict(self,features):
+        #sign(x.w+b)
+        classification = np.sign(np.dot(np.array(features),self.w)+self.b)
+
+        return classification
+
+
